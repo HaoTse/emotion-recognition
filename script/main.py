c7c8d477b2561a582fe68005c642724c939d7e63
HaoTse <top30339@gmail.com>
2017-08-28 23:26:17 +0800

    Change the 5-fold grouping way in script/main.py
    - Make it more average.

--- script/main.py
+++ script/main.py
@@ -1,17 +1,17 @@
 import sys
 from os import listdir
-import math
 
 speakers = ["03", "08", "09", "10", "11", "12", "13", "14", "15", "16"]
 emotions = ["W", "L", "E", "A", "F", "T", "N"]
 emoFile = [[], [], [], [], [], [], []]
-fivefold = [[], [], [], [], []]
+sortFile = []
+maxLen = 0
 
 svmPath = "svm"
 tenfoldPath = "10fold"
 fivefoldPath = "5fold"
 files = [f for f in listdir(svmPath)]
-"""
+
 # 10-fold
 for speaker in speakers:
     trainTxt = ""
@@ -28,28 +28,30 @@ for speaker in speakers:
         fout.write(testTxt)
     with open(tenfoldPath + "/train" + speaker + ".txt", 'w') as fout:
         fout.write(trainTxt)
-"""
+
 # 5-fold
 for f in files:
     for emotion in emotions:
         if emotion == f[5]:
             index = emotions.index(emotion)
             emoFile[index].append(f)
+            maxLen = len(emoFile[index]) if len(emoFile[index]) > maxLen else maxLen
             break
-for i in range(5):
-    for k in range(len(emoFile)):
-        start = int(math.floor(len(emoFile[k]) / 5 * i))
-        end = len(emoFile[k]) if i == 4 else int(math.ceil(len(emoFile[k]) / 5 * (i + 1)))
-        for j in range(start, end):
-            fivefold[i].append(emoFile[k][j])
+for i in range(maxLen):
+    for emof in emoFile:
+        if i < len(emof):
+            sortFile.append(emof[i])
 for i in range(5):
     trainTxt = ""
     testTxt = ""
-    for f in files:
+    startIdx = round(len(sortFile) / 5 * i)
+    endIdx = round(len(sortFile) / 5 * (i + 1))
+    for j in range(len(sortFile)):
+        f = sortFile[j]
         with open(svmPath + "/" + f, 'r') as fin:
             lines = fin.readlines()
         for line in lines:
-            if f in fivefold[i]:
+            if j >= startIdx and j < endIdx:
                 testTxt += (line)
             else:
                 trainTxt += (line)
