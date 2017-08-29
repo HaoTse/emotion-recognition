4596d942e1d0f3a538e09a6a0a997e6a327d0797
HaoTse <top30339@gmail.com>
2017-08-29 17:14:21 +0800

    Restart experiment
    - Update the output.sh to restore the result files.

--- output.sh
+++ output.sh
@@ -1,2 +1,2 @@
-svm-predict -b 1 train.scale train.scale.model Result_train.txt > result/$1/Accuracy_train$2.txt
-svm-predict -b 1 test.scale train.scale.model Result_test.txt > result/$1/Accuracy_test$2.txt
+svm-predict -b 1 train.scale train.scale.model result/$1/Result_train$2.txt > accuracy/$1/Accuracy_train$2.txt
+svm-predict -b 1 test.scale train.scale.model result/$1/Result_test$2.txt > accuracy/$1/Accuracy_test$2.txt
